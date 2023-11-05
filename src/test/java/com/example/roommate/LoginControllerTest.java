1a499be56691a54e4fd43f511ffa14cf65b67d43
Timm Krause <Timm.Krause@hhu.de>
2023-11-05 16:20:15 +0100

    add LoginControllerTest and tests for POST request

--- src/test/java/com/example/roommate/LoginControllerTest.java
+++ src/test/java/com/example/roommate/LoginControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
