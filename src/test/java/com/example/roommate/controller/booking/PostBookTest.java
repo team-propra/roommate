b130948c028855802bd4af4a9cafb2cf458ca55c
Daniel Mass <business.daniel.mass@gmail.com>
2023-11-08 20:29:22 +0100

    fix failing PostBookTest.java by changing the RequestBody Signature. Note: It is disgusting to keep adjusting the signature using param() just like that. There needs to be a better alternative

--- src/test/java/com/example/roommate/controller/booking/PostBookTest.java
+++ src/test/java/com/example/roommate/controller/booking/PostBookTest.java
@@ -1,12 +1,12 @@
 package com.example.roommate.controller.booking;
 
 
+import com.example.roommate.domain.values.BookDataForm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,7 +20,10 @@ public class PostBookTest {
     @Test
     @DisplayName("Its possible to POST /book")
     void test_2() throws Exception {
-        mvc.perform(post("/book"))
+        BookDataForm bookDataForm = new BookDataForm(5,true);
+        mvc.perform(post("/book")
+                        .param("roomID", Integer.toString(bookDataForm.roomID()))
+                        .param("Monday19", Boolean.toString(bookDataForm.Monday19())))
                 .andExpect(status().isCreated());
     }
 }
