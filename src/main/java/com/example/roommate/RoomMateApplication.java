d1a67355bc53274a7637ab83f7012f9ee077fd57
Daniel Mass <business.daniel.mass@gmail.com>
2023-11-08 15:27:53 -0500

    Tests in PostBookTest.java created Not implemented jet

--- src/main/java/com/example/roommate/RoomMateApplication.java
+++ src/main/java/com/example/roommate/RoomMateApplication.java
@@ -2,10 +2,8 @@ package com.example.roommate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.roommate")
 public class RoomMateApplication {
 
     public static void main(String[] args) {
