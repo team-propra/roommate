83994522b93581f205ba14270d94c6825e70fe4d
Daniel Mass <business.daniel.mass@gmail.com>
2023-11-08 21:25:24 +0100

    Abstracted Thymeleaf Testing using ThymeleafTestEngine.java and TestModel.java to resemble a simple testing environment with a similar interface as spring controllers

---
+++ src/test/java/com/example/roommate/frontend/HomeTest.java
@@ -0,0 +1,25 @@
+package com.example.roommate.frontend;
+
+import com.example.roommate.frontend.utility.ThymeleafTestEngine;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Description;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HomeTest {
+    
+    @Test
+    @DisplayName("Check, that home.html contains the string 'Homepage'")
+    @Description("this is a bad test for illustration purposes, as we dont use thymeleaf yet")
+    public void test() {
+        //Arrange
+        ThymeleafTestEngine thymeleafTestEngine = new ThymeleafTestEngine();
+        
+        //Act
+        String render = thymeleafTestEngine.render("home.html");
+        
+        //Assert
+        assertThat(render).contains("Homepage");
+    }
+}
