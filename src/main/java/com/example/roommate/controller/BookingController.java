fcdf8fc78d18df248c9ce43fcdb3b7b25270423d
nighoge <till.schellscheidt@web.de>
2023-11-16 05:30:13 -0500

    removing unnecessary import statements

--- src/main/java/com/example/roommate/controller/BookingController.java
+++ src/main/java/com/example/roommate/controller/BookingController.java
@@ -4,13 +4,11 @@ import com.example.roommate.domain.entities.Room;
 import com.example.roommate.domain.values.BookDataForm;
 import com.example.roommate.services.BookEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.UUID;
 
@@ -48,13 +46,12 @@ public class BookingController {
     @PostMapping("/book")
     public String addBooking(Model model, BookDataForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if(bindingResult.hasErrors()){
-            return "addroom";
+            return "/book";
         }
         System.out.println(form);
 
         //view.setStatusCode(HttpStatus.CREATED);
         bookEntryService.addBookEntry(form);
-        //model.addAttribute("form", form);
         redirectAttributes.addFlashAttribute("success", "Buchung erfolgreich hinzugefügt.");
         return "redirect:/home";
     }
--- src/main/java/com/example/roommate/controller/HomeController.java
+++ src/main/java/com/example/roommate/controller/HomeController.java
@@ -1,15 +1,12 @@
 package com.example.roommate.controller;
 
-import com.example.roommate.domain.values.BookDataForm;
 import com.example.roommate.services.BookEntryService;
-import com.example.roommate.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller()
 @RequestMapping("/home")
