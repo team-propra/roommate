48bdf530ce5088f2d50ba4dab8087419561dbec4
Timm Krause <Timm.Krause@hhu.de>
2023-11-22 15:55:57 +0100

    added @NotBlank annotation to BookDataForm

--- src/main/java/com/example/roommate/domain/values/BookDataForm.java
+++ src/main/java/com/example/roommate/domain/values/BookDataForm.java
@@ -1,9 +1,11 @@
 package com.example.roommate.domain.values;
 
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.lang.NonNull;
+import jakarta.validation.constraints.AssertTrue;
 
 
 import java.util.UUID;
 
-public record BookDataForm(@NonNull  UUID roomID, /*@AssertTrue*/ boolean Monday19 ){}
+public record BookDataForm(@NotBlank @NonNull  UUID roomID, /*@AssertTrue*/ boolean Monday19 ){}
