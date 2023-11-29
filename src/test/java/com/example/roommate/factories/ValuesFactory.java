1d4c9c3eb14ebb4889f0828fafc252b7de546eee
Daniel Mass <business.daniel.mass@gmail.com>
2023-11-29 12:32:21 +0100

    fix(ValuesFactory): Form id changed to String

--- src/test/java/com/example/roommate/factories/ValuesFactory.java
+++ src/test/java/com/example/roommate/factories/ValuesFactory.java
@@ -11,10 +11,10 @@ public class ValuesFactory {
 
     public static UUID id = UUID.fromString("9e255449-449b-4564-8bc0-5e4517708364");
     public static BookDataForm createBookDataForm() {
-        return new BookDataForm(id, true);
+        return new BookDataForm(id.toString(), true);
     }
     public static BookDataForm createInvalidBookDataForm() {
-        return new BookDataForm(id, false);
+        return new BookDataForm(id.toString(), false);
     }
 
     public static Item createItem() {
