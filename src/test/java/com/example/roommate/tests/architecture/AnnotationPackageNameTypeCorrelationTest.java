e2150fffa68e3e3c10df29b1365b53f67f8104de
Daniel Mass <business.daniel.mass@gmail.com>
2023-12-02 20:45:05 +0100

    refactor(architectureTests): classCorrelations generate dynamic tests, problem: this approach assumes too much

--- src/test/java/com/example/roommate/tests/architecture/AnnotationPackageNameTypeCorrelationTest.java
+++ src/test/java/com/example/roommate/tests/architecture/AnnotationPackageNameTypeCorrelationTest.java
@@ -2,56 +2,34 @@ package com.example.roommate.tests.architecture;
 
 import com.example.roommate.annotations.ApplicationService;
 import com.example.roommate.annotations.DomainService;
-import com.sun.tools.javac.Main;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.syntax.elements.*;
-import org.junit.jupiter.api.DisplayName;
+import com.tngtech.archunit.lang.conditions.ArchConditions;
+import org.apache.catalina.startup.ClassLoaderFactory;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.springframework.stereotype.Repository;
 
-import java.lang.annotation.Annotation;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.example.roommate.tests.architecture.utility.Correlations.classCorrelations;
 
 
 
 public class AnnotationPackageNameTypeCorrelationTest {
     @TestFactory
     public Stream<DynamicTest> domainServices(){
-        return correlation("..roommate.domain.services.." ,DomainService.class, ClassesThat::areNotInterfaces, given->given.haveSimpleNameEndingWith("DomainService"));
+        return classCorrelations("..roommate.domain.services.." ,DomainService.class, ArchConditions.haveSimpleNameEndingWith("DomainService"));
     }
 
     @TestFactory
     public Stream<DynamicTest> applicationServices(){
-        return correlation("..roommate.services..", ApplicationService.class, ClassesThat::areNotInterfaces, given->given.haveSimpleNameEndingWith("Service"));
+        return classCorrelations("..roommate.services..", ApplicationService.class, ArchConditions.haveSimpleNameEndingWith("Service"));
     }
-    
-    static Stream<DynamicTest> correlation(String packageIdentifier, Class<? extends Annotation> annotation, Function<ClassesThat<GivenClassesConjunction>,GivenClassesConjunction> typeRequirement, Function<ClassesShould,ClassesShouldConjunction> nameRequirement){
-        JavaClasses classes =
-                new ClassFileImporter().importPackages("com.example.roommate");
-        String annotationName = annotation.getSimpleName();
-        
-        ArchRule annotationPackage = classes()
-            .that()
-            .areAnnotatedWith(annotation)
-            .should()
-            .resideInAPackage(packageIdentifier);
-        
-        ArchRule packageAnnotation = classes()
-                .that()
-                .resideInAPackage(packageIdentifier)         
-                .should()
-                .beAnnotatedWith(annotation);
-        
-        return Stream.of(
-                DynamicTest.dynamicTest(annotationName+" correlates to package: "+packageIdentifier, ()->annotationPackage.check(classes)),
-                DynamicTest.dynamicTest("package "+packageIdentifier+" correlates to annotation: "+annotationName, ()->packageAnnotation.check(classes))
-        );
+
+    @TestFactory
+    public Stream<DynamicTest> repositories(){
+        return classCorrelations("..roommate.persistence..", Repository.class, ArchConditions.haveSimpleNameEndingWith("Repository"));
     }
-    
+
+
+
 }
---
+++ src/test/java/com/example/roommate/tests/architecture/utility/Correlations.java
@@ -0,0 +1,62 @@
+package com.example.roommate.tests.architecture.utility;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.elements.ClassesShould;
+import com.tngtech.archunit.lang.syntax.elements.ClassesShouldConjunction;
+import com.tngtech.archunit.lang.syntax.elements.ClassesThat;
+import com.tngtech.archunit.lang.syntax.elements.GivenClassesConjunction;
+import org.junit.jupiter.api.DynamicTest;
+
+import java.lang.annotation.Annotation;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class Correlations {
+    
+    static ClassesThat<GivenClassesConjunction> actuallyClasses = classes()
+            .that()
+            .areNotInterfaces()
+            .and();
+
+    static ClassesThat<GivenClassesConjunction> actuallyInterfaces = classes()
+            .that()
+            .areInterfaces()
+            .and()
+            .areNotAnnotations()
+            .and();
+
+    static ClassesThat<GivenClassesConjunction> actuallyAnnotations = classes()
+            .that()
+            .areAnnotations()
+            .and();
+    public static Stream<DynamicTest> classCorrelations(String packageIdentifier, Class<? extends Annotation> annotation, ArchCondition<JavaClass> nameRequirement){
+        JavaClasses classes =
+                new ClassFileImporter().importPackages("com.example.roommate");
+        String annotationName = annotation.getSimpleName();
+
+        ArchRule annotationPackage = actuallyClasses
+                .areAnnotatedWith(annotation)
+                .should()
+                .resideInAPackage(packageIdentifier);
+
+        ArchRule annotationNaming = actuallyClasses
+                .areAnnotatedWith(annotation)
+                .should(nameRequirement);
+
+        ArchRule packageAnnotation = actuallyClasses
+                .resideInAPackage(packageIdentifier)
+                .should()
+                .beAnnotatedWith(annotation);
+
+        return Stream.of(
+                DynamicTest.dynamicTest("annotation: <%s> correlates to package: <%s>".formatted(annotationName,packageIdentifier), ()->annotationPackage.check(classes)),
+                DynamicTest.dynamicTest("package: <%s> correlates to annotation: <%s>".formatted(packageIdentifier,annotationName), ()->packageAnnotation.check(classes))
+        );
+    }
+}
