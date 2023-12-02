9e376b9bcea0425e68bba38eb082151a59611341
Daniel Mass <business.daniel.mass@gmail.com>
2023-12-02 21:49:53 +0100

    test(namingToAnnotation): added support for name-to-* correlation
    faced some difficulties converting ArchCondition<JavaClass> into DescribedPredicate<JavaClass>

---
+++ src/test/java/com/example/roommate/tests/architecture/utility/ArchConditionToDescribedPredicateAdapter.java
@@ -0,0 +1,29 @@
+package com.example.roommate.tests.architecture.utility;
+
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ConditionEvents;
+
+public class ArchConditionToDescribedPredicateAdapter extends DescribedPredicate<JavaClass> {
+    private final ArchCondition<JavaClass> archCondition;
+
+    ArchConditionToDescribedPredicateAdapter(ArchCondition<JavaClass> archCondition) {
+        super(archCondition.getDescription());
+        this.archCondition = archCondition;
+    }
+    public static DescribedPredicate<JavaClass> create(ArchCondition<JavaClass> condition){
+        return new ArchConditionToDescribedPredicateAdapter(condition);
+    }
+
+    @Override
+    public boolean test(JavaClass javaClass) {
+        ConditionEvents conditionEvents = ConditionEvents.Factory.create();
+        archCondition.check(javaClass, conditionEvents);
+        return !conditionEvents.containViolation();
+    }
+    
+    
+
+    // You might need to override other methods like asString() if necessary
+}
--- src/test/java/com/example/roommate/tests/architecture/utility/Correlations.java
+++ src/test/java/com/example/roommate/tests/architecture/utility/Correlations.java
@@ -1,10 +1,12 @@
 package com.example.roommate.tests.architecture.utility;
 
+import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.syntax.elements.ClassesShould;
 import com.tngtech.archunit.lang.syntax.elements.ClassesShouldConjunction;
 import com.tngtech.archunit.lang.syntax.elements.ClassesThat;
@@ -16,6 +18,7 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
 
 public class Correlations {
     
@@ -54,9 +57,24 @@ public class Correlations {
                 .should()
                 .beAnnotatedWith(annotation);
 
+        ArchRule packageNaming = actuallyClasses
+                .resideInAPackage(packageIdentifier)
+                .should(nameRequirement);
+
+
+        ArchRule namingAnnotation = actuallyClasses
+                .areNotInterfaces()
+                .and(ArchConditionToDescribedPredicateAdapter.create(nameRequirement))
+                .should()
+                .beAnnotatedWith(annotation);
+
         return Stream.of(
                 DynamicTest.dynamicTest("annotation: <%s> correlates to package: <%s>".formatted(annotationName,packageIdentifier), ()->annotationPackage.check(classes)),
-                DynamicTest.dynamicTest("package: <%s> correlates to annotation: <%s>".formatted(packageIdentifier,annotationName), ()->packageAnnotation.check(classes))
+                DynamicTest.dynamicTest("annotation: <%s> correlates to naming: <%s>".formatted(annotationName,nameRequirement), ()->annotationNaming.check(classes)),
+                DynamicTest.dynamicTest("package: <%s> correlates to annotation: <%s>".formatted(packageIdentifier,annotationName), ()->packageAnnotation.check(classes)),
+                DynamicTest.dynamicTest("package: <%s> correlates to naming: <%s>".formatted(packageIdentifier,nameRequirement), ()->packageNaming.check(classes)),
+                DynamicTest.dynamicTest("naming: <%s> correlates to annotation: <%s>".formatted(nameRequirement,annotationName), ()->namingAnnotation.check(classes))
+                
         );
     }
 }
