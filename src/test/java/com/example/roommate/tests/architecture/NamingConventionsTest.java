375bcb20d8f2d47e29937bdf4d711f7736089854
Daniel Mass <business.daniel.mass@gmail.com>
2023-12-02 17:50:26 +0100

    test(services): naming convention: ending with Suffix "Services"

---
+++ src/test/java/com/example/roommate/tests/architecture/NamingConventionsTest.java
@@ -0,0 +1,20 @@
+package com.example.roommate.tests.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Service;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.example.roommate")
+public class NamingConventionsTest {
+    @ArchTest
+    static ArchRule services = classes()
+            .that()
+            .resideInAPackage("com.example.roommate.services..")
+            .and()
+            .areAnnotatedWith(Service.class)
+            .should()
+            .haveSimpleNameEndingWith("Service");
+}
