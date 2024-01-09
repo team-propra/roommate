8c2e74134e2ab98ba31f33b80e0df4a5f57068f7
Timm Krause <Timm.Krause@hhu.de>
2024-01-09 16:30:53 +0100

    feat: added oauth to login method (bug-fixes still necessary)

--- src/main/java/com/example/roommate/config/SecurityConfig.java
+++ src/main/java/com/example/roommate/config/SecurityConfig.java
@@ -3,14 +3,9 @@ package com.example.roommate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -19,22 +14,28 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
         chainBuilder
                 .authorizeHttpRequests(configurer -> configurer
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(loginConfigurer -> loginConfigurer
+                /*.formLogin(loginConfigurer -> loginConfigurer
                         .defaultSuccessUrl("/", true)
-                        .permitAll());
+                        .permitAll())
+
+                 */
+                //.formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
 
         return chainBuilder.build();
     }
 
-
+    /*
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(new User("user", "{noop}1234", List.of()));
         return manager;
     }
+
+     */
 
 
 }
