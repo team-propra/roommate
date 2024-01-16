35ccf61d275d95a2748e57090ef8b5cbb4cbce68
Felix Loch <Felix.Loch@hhu.de>
2024-01-16 09:27:58 +0100

    fix: added timeperoid of bookings on home

--- src/main/java/com/example/roommate/controller/HomeController.java
+++ src/main/java/com/example/roommate/controller/HomeController.java
@@ -4,11 +4,13 @@ import com.example.roommate.application.services.BookingApplicationService;
 import com.example.roommate.exceptions.applicationService.NotFoundException;
 import com.example.roommate.interfaces.entities.IBooking;
 import com.example.roommate.interfaces.entities.IRoom;
+import com.example.roommate.values.domainValues.BookingDays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -31,8 +33,16 @@ public class HomeController {
                 throw new RuntimeException(e);
             }
         }).toList();
+        List<String> timeframes = new ArrayList<>();
+        for (IBooking booking: bookEntries) {
+            BookingDays bookingDays = booking.getBookingDays();
+            timeframes.add(bookingDays.convertToString());
+            System.out.println(bookingDays.convertToString());
+        }
         model.addAttribute("bookings", bookEntries);
         model.addAttribute("rooms", rooms);
+        model.addAttribute("timeframes", timeframes);
+
         return "home";
 
     }
