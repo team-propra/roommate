7d46cf2eb81af1955310e1d923a7b02172d605ac
Daniel Mass <business.daniel.mass@gmail.com>
2024-01-28 19:20:04 +0100

    fix: V1__ ... flyway

--- src/main/resources/db/migration/V1__create_room.sql
+++ src/main/resources/db/migration/V1__create_room.sql
@@ -12,14 +12,11 @@ CREATE TABLE booked_timeframe (
     day_of_week VARCHAR(255) NOT NULL,
     local_time TIME NOT NULL,
     duration INTERVAL NOT NULL,
-    room_id UUID NOT NULL,
-    FOREIGN KEY room_id REFERENCES room(id)
+    room_id UUID NOT NULL REFERENCES room(id)
 );
 
 CREATE TABLE item_to_room (
     id UUID PRIMARY KEY,
-    item_name VARCHAR(255) NOT NULL,
-    room_id UUID NOT NULL,
-    FOREIGN KEY room_id REFERENCES room(id),
-    FOREIGN KEY item_name REFERENCES item(item_name)
+    item_name VARCHAR(255) NOT NULL REFERENCES item(item_name),
+    room_id UUID NOT NULL REFERENCES room(id)
 )
