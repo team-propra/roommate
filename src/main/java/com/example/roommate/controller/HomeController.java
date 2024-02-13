75214286df86af30f5dc71bee6be6e2cfb0fa498
nighoge <till.schellscheidt@web.de>
2024-02-13 04:21:12 -0500

    feat: /createItem works on adminPage.html

--- src/main/java/com/example/roommate/controller/HomeController.java
+++ src/main/java/com/example/roommate/controller/HomeController.java
@@ -2,13 +2,19 @@ package com.example.roommate.controller;
 
 import com.example.roommate.annotations.AdminOnly;
 import com.example.roommate.application.services.BookingApplicationService;
+import com.example.roommate.interfaces.entities.IRoom;
 import com.example.roommate.values.domainValues.DayTimeFrame;
+import com.example.roommate.values.domainValues.ItemName;
 import com.example.roommate.values.models.RoomHomeModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Collection;
 import java.util.List;
 
 
@@ -32,7 +38,27 @@ public class HomeController {
 
     @AdminOnly
     @GetMapping("/edit")
-    public String adminPage() {
+    public String adminPage(Model model) {
+        Collection<ItemName> itemList = bookingApplicationService.getItems();
+        Collection<IRoom> roomList = bookingApplicationService.getRooms();
+
+        model.addAttribute("itemList", itemList);
+        model.addAttribute("roomList", roomList);
         return "adminEdit";
     }
+
+    @AdminOnly
+    @PostMapping("/createItem")
+    public String createItem(Model model, @RequestParam String newItem) {
+        bookingApplicationService.createItem(newItem);
+        return adminPage(model);
+    }
+
+    @AdminOnly
+    @PostMapping("/deleteItem/{itemName}")
+    public String deleteItem(Model model, @PathVariable String itemName) {
+        //bookingApplicationService.removeItem(itemName); // have to iterate through all workspaces to maintian consistency
+        return adminPage(model);
+    }
+
 }
