0922740259c74652c1b182c3b7c49153ceacd946
Daniel Mass <business.daniel.mass@gmail.com>
2024-02-15 23:17:59 +0100

    fix: postgres RoomRepository flyway errors

--- src/main/java/com/example/roommate/persistence/postgres/WorkspacesDTO.java
+++ src/main/java/com/example/roommate/persistence/postgres/WorkspacesDTO.java
@@ -1,8 +1,10 @@
 package com.example.roommate.persistence.postgres;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
 
+@Table("workspace")
 public record WorkspacesDTO (@Id UUID id, int workspaceNumber, UUID roomId){
 }
