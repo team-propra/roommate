9b482b18692da2e497ba6ff7e6d83d0a9d8479b8
Daniel Mass <business.daniel.mass@gmail.com>
2024-02-17 15:00:40 +0100

    fix: recover lost merge conflict artifact

--- src/main/java/com/example/roommate/values/domainValues/BookingDays.java
+++ src/main/java/com/example/roommate/values/domainValues/BookingDays.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public record BookingDays(
         int stepSize,
@@ -27,6 +28,14 @@ public record BookingDays(
    private static ArrayList<Boolean> CreateListOfSize(int stepSize)
    {
       return new ArrayList<>(Collections.nCopies(getListSize(stepSize), false));
+   }
+
+   public static boolean validateBookingCoorectness(BookingDays bookingDays) {
+      //At least one box is set to true
+      Stream<List<Boolean>> bookingStream = Stream.of(IterableSupport.toList(bookingDays.mondayBookings), IterableSupport.toList(bookingDays.tuesdayBookings), IterableSupport.toList(bookingDays.wednesdayBookings), IterableSupport.toList(bookingDays.thursdayBookings), IterableSupport.toList(bookingDays.fridayBookings), IterableSupport.toList(bookingDays.saturdayBookings), IterableSupport.toList(bookingDays.sundayBookings));
+      Stream<Boolean> combinedStream = bookingStream.flatMap(List::stream);
+      // stream.forEach(System.out::println);
+      return combinedStream.anyMatch(value -> value.equals(true));
    }
    BookingDays(int stepSize){
       this(
