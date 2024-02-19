f7ba1728ab72ab8f25c969b345f5ea632408425c
Timm Krause <Timm.Krause@hhu.de>
2024-02-19 16:58:59 +0100

    feat: added admin .env variable. Put your GitHubHandle into the .env and you now have admin privileges!

--- src/main/java/com/example/roommate/application/services/AuthenticationApplicationService.java
+++ src/main/java/com/example/roommate/application/services/AuthenticationApplicationService.java
@@ -5,6 +5,7 @@ import com.example.roommate.domain.services.UserDomainService;
 import com.example.roommate.interfaces.application.services.IAuthenticationApplicationService;
 import com.example.roommate.interfaces.entities.IUser;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -13,7 +14,6 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,6 +23,9 @@ import java.util.UUID;
 @SuppressFBWarnings(value="EI2", justification="UserDomainService is properly injected")
 public class AuthenticationApplicationService implements OAuth2UserService<OAuth2UserRequest, OAuth2User>, IAuthenticationApplicationService {
     UserDomainService userDomainService;
+
+    @Value("${roommate.admin-handle}")
+    String adminHandle;
 
     public AuthenticationApplicationService(UserDomainService userDomainService) {
         this.userDomainService = userDomainService;
@@ -36,6 +39,13 @@ public class AuthenticationApplicationService implements OAuth2UserService<OAuth
         Set<GrantedAuthority> authorities = new HashSet<>(originalUser.getAuthorities());
 
         String login = originalUser.getAttribute("login");
+        assert login != null;
+        if(login.equals(adminHandle)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            userDomainService.addAdmin(login);
+            return new DefaultOAuth2User(authorities, originalUser.getAttributes(), "id");
+        }
+
         String userRole;
         IUser userByLogin = getUserByLogin(login);
         if (userByLogin != null) {
@@ -53,16 +63,6 @@ public class AuthenticationApplicationService implements OAuth2UserService<OAuth
     
     public IUser getUserByLogin(String login) {
         return userDomainService.getUserByLogin(login);
-    }
-
-    public boolean isVerified(String login) {
-        if(getUserByLogin(login) == null) {
-            userDomainService.addUser(login);
-            return false;
-        }
-        else {
-            return getUserByLogin(login).getRole().equals("VERIFIED_USER");
-        }
     }
 
     public void registerKey(UUID keyId, String login) {
