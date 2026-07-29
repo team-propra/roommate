346e36bc1fd1e68e5b36eea24f820735145671ed
Felix Loch <Felix.Loch@hhu.de>
2026-07-29 15:00:09 +0200

    fix(user): spring security using database roles correctly

--- src/main/java/com/example/roommate/config/WebConfiguration.java
+++ src/main/java/com/example/roommate/config/WebConfiguration.java
@@ -1,25 +1,12 @@
 package com.example.roommate.config;
 
-import com.example.roommate.interceptors.VerifyAccessInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
-
-    @Autowired
-    private VerifyAccessInterceptor verifyAccessInterceptor;
-
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(verifyAccessInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/js/**", "/images/**", "/webjars/**");
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
